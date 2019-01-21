16930eb7f58b63d31ba3b0c4dac4860f16cf162c
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-01-21 19:01:39 +1100

    Add props to BandDto

--- server/Models/Band/BandDto.cs
+++ server/Models/Band/BandDto.cs
@@ -2,6 +2,16 @@ namespace Furzify.API.Models.Band
 {
     public class BandDto
     {
-
+        public string Name { get; set; }
+        public string AppleMusic { get; set; }
+        public string Bandcamp { get; set; }
+        public string Facebook { get; set; }
+        public string Instagram { get; set; }
+        public string ReverbNation { get; set; }
+        public string SoundCloud { get; set; }
+        public string Spotify { get; set; }
+        public string Twitter { get; set; }
+        public string Website { get; set; }
+        public string YouTube { get; set; }
     }
 }
