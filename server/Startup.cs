30686cea883c71224ad5696cba0787dc92d053c4
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-01-21 18:22:59 +1100

    Add BandService to service collection

--- server/Startup.cs
+++ server/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Furzify.API.Interfaces;
+using Furzify.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -30,7 +32,7 @@ namespace Furzify.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
+            services.AddScoped<IBandService, BandService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
