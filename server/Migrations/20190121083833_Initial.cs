b41818a0ad278b28305cf84d9494b5c662b731f6
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-01-21 19:39:47 +1100

    Add initial migration

---
+++ server/Migrations/20190121083833_Initial.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Furzify.API.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true),
+                    AppleMusic = table.Column<string>(nullable: true),
+                    Bandcamp = table.Column<string>(nullable: true),
+                    Facebook = table.Column<string>(nullable: true),
+                    Instagram = table.Column<string>(nullable: true),
+                    ReverbNation = table.Column<string>(nullable: true),
+                    SoundCloud = table.Column<string>(nullable: true),
+                    Spotify = table.Column<string>(nullable: true),
+                    Twitter = table.Column<string>(nullable: true),
+                    Website = table.Column<string>(nullable: true),
+                    YouTube = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bands", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bands");
+        }
+    }
+}
