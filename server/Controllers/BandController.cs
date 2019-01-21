6f73c70935b4f25a08e0d5ede03f1313153d511b
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-01-21 18:23:48 +1100

    inject into controller

--- server/Controllers/BandController.cs
+++ server/Controllers/BandController.cs
@@ -1,10 +1,16 @@
+using Furzify.API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Furzify.API.Controllers
 {
     public class BandController : Controller
     {
-        public BandController() { }
+        private readonly IBandService _bandService;
+
+        public BandController(IBandService bandService)
+        {
+            _bandService = bandService;
+        }
 
         public IActionResult Index()
         {
