cda58498fc274041daf5c7789f951d66bf153d04
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-01-21 20:07:35 +1100

    add songs table migration

---
+++ server/Migrations/20190121090712_AddSongsTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Furzify.API.Migrations
+{
+    public partial class AddSongsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    BandId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Spotify = table.Column<string>(nullable: true),
+                    YouTube = table.Column<string>(nullable: true),
+                    YouTubeMusic = table.Column<string>(nullable: true),
+                    AppleMusic = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Songs_Bands_BandId",
+                        column: x => x.BandId,
+                        principalTable: "Bands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_BandId",
+                table: "Songs",
+                column: "BandId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Songs");
+        }
+    }
+}
